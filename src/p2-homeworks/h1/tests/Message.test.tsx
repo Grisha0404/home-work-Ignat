dae43c8e27ea5b8dbd76955953fe70b9699fbdc7
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-04 12:57:18 +0300

    Update Message.test.tsx

--- src/p2-homeworks/h1/tests/Message.test.tsx
+++ src/p2-homeworks/h1/tests/Message.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {render} from '@testing-library/react'
 import Message from '../Message'
+import '@testing-library/jest-dom/extend-expect';
 
 test('find text "test message name"',
     () => {
