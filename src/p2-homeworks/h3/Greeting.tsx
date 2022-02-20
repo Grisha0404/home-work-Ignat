4b4c21f6a6b4b2426d55619d13854a6ef990c032
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-20 12:53:17 +0300

    Update Greeting.tsx

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -5,22 +5,20 @@ type GreetingPropsType = {
     name: string // need to fix any
     setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
     addUser: () => void // need to fix any
-    error:(name: string) => void // need to fix any
+    error: string // need to fix any
     totalUsers: number// need to fix any
 }
 
-// презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
-
+    const inputClass = error ? s.error : s.someClass; // need to fix with (?:)
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.greeting}>
+            <input value={name} onChange={setNameCallback} className={inputClass} placeholder={'Name'}/>
+            <span className={s.errorText}>{error}</span>
+            <button onClick={addUser} className={s.button}>Add</button>
+            <span className={s.total}>{totalUsers}</span>
         </div>
     )
 }
