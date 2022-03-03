be716c722faf69465e9e7729b33f9463a328b8f7
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-03 16:40:41 +0300

    Updates

---
+++ precache-manifest.8c6208f438eaa61da25ed7e402bb532d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b382d8ce60041fd19a7788fc5f28c18e",
+    "url": "/home-work-Ignat/index.html"
+  },
+  {
+    "revision": "178dfcbcfca9847e0ce1",
+    "url": "/home-work-Ignat/static/css/main.79c3d254.chunk.css"
+  },
+  {
+    "revision": "69819d1ab46c2efc5285",
+    "url": "/home-work-Ignat/static/js/2.cefda22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-work-Ignat/static/js/2.cefda22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829fb92d57c6dfc27baf",
+    "url": "/home-work-Ignat/static/js/3.ff09556f.chunk.js"
+  },
+  {
+    "revision": "178dfcbcfca9847e0ce1",
+    "url": "/home-work-Ignat/static/js/main.c452142f.chunk.js"
+  },
+  {
+    "revision": "c8c45f7b21e2139744f4",
+    "url": "/home-work-Ignat/static/js/runtime-main.1b4dca40.js"
+  }
+]);
