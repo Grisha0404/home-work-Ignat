52b9c04c64565603a17f629f427a4648999ebee5
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-03 21:03:38 +0300

    Create AddLink.tsx

---
+++ src/p2-homeworks/h5/pages/AddLink.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import s from './hw5.module.css'
+
+export const AddLink = () => {
+    return (
+        <div className={s.link}>
+            <Link to='/pre-junior'>pre-junior</Link>
+            <Link to='/junior'>junior</Link>
+            <Link to='/junior-plus'>junior-plus</Link>
+        </div>
+    );
+};
