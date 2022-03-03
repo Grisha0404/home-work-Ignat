5dc7a9631a9a86894aac8631b771f58976ca334b
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-03 21:03:57 +0300

    Update Routes.tsx

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import {Redirect, Switch} from "react-router-dom";
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
+import {Route, Redirect, Switch} from "react-router-dom";
+import {Junior} from "./pages/Junior";
+import {JuniorPlus} from "./pages/JuniorPlus";
+
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus'
     // add paths
 }
 
@@ -14,15 +19,15 @@ function Routes() {
             {/*Switch выбирает первый подходящий роут*/}
             <Switch>
 
-            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR
+            exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
+                <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
 
-            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
-                // add routes
-
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            <Route render={() => <Error404/>}/>
+                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+                <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
+                {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route render={() => <Error404/>}/>
 
             </Switch>
         </div>
