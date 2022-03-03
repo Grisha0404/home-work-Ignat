5f34e921bf87d728bd755d7ef444c7b1fb7c3282
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-03 21:03:55 +0300

    Create JuniorPlus.tsx

---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HW12 from "../../h12/HW12";
+import HW10 from "../../h10/HW10";
+import HW11 from "../../h11/HW11";
+import Error404 from "../../h5/pages/Error404";
+
+export const JuniorPlus = () => {
+    return (
+        <div>
+            <Error404/>
+           {/* <HW10/>
+            <HW11/>
+            <HW12/>*/}
+        </div>
+    );
+};
