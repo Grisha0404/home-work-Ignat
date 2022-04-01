814eec3bd350665309b674600222585e2ba3de2d
grisha0404 <grisha.kom@gmail.com>
2022-04-01 11:17:46 +0300

    change

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,6 +1,11 @@
 import {UserType} from "../HW8";
 
-export const homeWorkReducer = (state: UserType[], action: any): any => { // need to fix any
+type ActionType =
+    { type: 'sortUp', payload: 'up' }
+    | { type: 'sortDown', payload: 'down' }
+    | { type: 'check', payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sortUp': {
             return [...state].sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)
@@ -9,7 +14,7 @@ export const homeWorkReducer = (state: UserType[], action: any): any => { // nee
             return [...state].sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
         }
         case 'check': {
-            return state.filter(el => el.age >= 18)
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
