e06c3f5b36cdd799a2a1c02cb455857073cd9912
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-01 13:31:43 +0300

    hw13

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {Request} from './Request/Request'
+import {RequestsAPI} from "./Request/RequestsAPI";
+
+export const HW13 = () => {
+    const [checked, setChecked] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
+
+    useEffect(() => {
+            RequestsAPI.postRequests(checked)
+                .then((res) => {
+                    setError('')
+                })
+                .catch((error) => {
+                    console.log({...error});
+                    setError(error.response ? error.response.data.errorText : error.message)
+                    console.log(error.response ? error.response.data.errorText : error.message);
+                })
+        },
+        [checked])
+
+    const click = () => {
+        setChecked(!checked)
+    }
+
+    return (
+        <div>
+            <Request checked={checked} click={click} error={error}/>
+        </div>
+    );
+};
